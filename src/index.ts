c295824442d02bb09543297acb0645cda03bc408
myamyang_works <dlekddl43@gmail.com>
2023-01-02 16:59:12 +0900

    #5.5 Typescript Blockchain, Blocks

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
-// myPackage.js → myPackage.d.ts → index.ts
-import { init, exit } from "./myPackage";
+import crypto from "crypto";
 
-init(false, "manju");
+// 🍥 hash, preHash (이전 Hash), height (블록의 Index 위치), data (블록이 보호할 Data)
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
 
-exit(10);
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+  }
+}
